be3f49f9caff76022d35315e87cbe47ca28a2ca3
Joseph Cumbo <mooman219@gmail.com>
2015-10-07 09:22:39 -0400

    Serve static files

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.use(express.static("public"));
+
+//app.get('/', function (req, res) {
+//    res.send('Hello World!');
+//});
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
