a24ebeb6e8b6457f999bf4baaaacb1d6c5603b4b
Joseph Cumbo <mooman219@gmail.com>
2015-10-07 14:48:31 -0400

    Setup sending/receiving data from client to server

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+
 var data = {};
+data.text = "Swag box";
 
-app.use(express.static("public"));
+app.use(bodyParser.json()); // Support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Support encoded bodies
 
-app.get("/random", function (req, res) {
+app.use(express.static("public")); // Mount public folder
+
+app.get("/random", function(req, res) {
    res.send(200, Math.random());
+});
+
+app.post("/text", function(req, res) {
+    console.log(req.body);
+    if (typeof req.body.input != 'undefined') {
+        data.text += "<br />" + req.body.input;
+    }
+    res.status(200).send(data.text);
+});
+
+app.get("/text", function(req, res) {
+    res.status(200).send(data.text);
 });
 
 var server = app.listen(3000, function () {
